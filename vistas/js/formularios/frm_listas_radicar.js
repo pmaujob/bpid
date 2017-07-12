0ed00d37a6ed71ffcb022e6722e944bf67ef1afe
Mauricio Hernandez <pmaujob@gmail.com>
2017-07-12 18:07:22 -0500

    Corrección listas

--- vistas/js/formularios/frm_listas_radicar.js
+++ vistas/js/formularios/frm_listas_radicar.js
@@ -1,65 +1,65 @@
-function onLoadBody(){
-    
-    $(document).ready(function(){
+function onLoadBody() {
+
+    $(document).ready(function () {
         // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
         $('.modal').modal();
-    });
-    
-    $(document).ready(function(){
-        $('.collapsible').collapsible();
     });
 
     $('.REQOBS').trigger('autoresize');
 
 }
 
-function buscarViabilidades(){
-    
+function buscarViabilidades() {
+
     var datos;
 
     value = document.getElementById("input_buscar").value;
-    
-    jQuery.ajax({	
-        type: 'POST',
-        url:'../../vistas/formulariosDinamicos/frmRadicados.php',
-        async: true,
-        data:{value:value},
-        success:function(respuesta){
 
-            document.getElementById('resultado').innerHTML='<p>'+ respuesta + '</p>';
+    jQuery.ajax({
+        type: 'POST',
+        url: '../../vistas/formulariosDinamicos/frmRadicados.php',
+        async: true,
+        data: {value: value},
+        success: function (respuesta) {
+
+            document.getElementById('resultado').innerHTML = '<p>' + respuesta + '</p>';
 
         },
 
         error: function () {
             alert("Error inesperado")
-            window.top.location ="../index.html";	
+            window.top.location = "../index.html";
         }
-        
+
     });
-    
+
 }
 
-function mas(cod){
-    
+function mas(cod) {
+
     $('#modal1').modal('open');
-    
+
     value = cod;
-    
-    jQuery.ajax({	
+
+    jQuery.ajax({
         type: 'POST',
-        url:'../../vistas/formulariosDinamicos/frmListas.php',
+        url: '../../vistas/formulariosDinamicos/frmListas.php',
         async: true,
-        data:{value:value},
-        success:function(respuesta){
+        data: {value: value},
+        success: function (respuesta) {
             
-            document.getElementById('collapsible').innerHTML='<p>'+ respuesta + '</p>';      
+            document.getElementById('collapsible').innerHTML = respuesta;
+
+            $(document).ready(function () {
+                $('.collapsible').collapsible();
+            });
 
         },
 
         error: function () {
             alert("Error inesperado")
-            window.top.location ="../index.html";	
-        } 
+            window.top.location = "../index.html";
+        }
     });
-    
+
 }
