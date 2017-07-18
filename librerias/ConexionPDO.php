1173d07a8eefe479f5d8f7619bba09df44fdc5a5
Daniel <danielernestodaza@hotmail.com>
2017-07-18 09:16:40 -0500

    Signed-off-by: Daniel <danielernestodaza@hotmail.com>

--- librerias/ConexionPDO.php
+++ librerias/ConexionPDO.php
@@ -1,9 +1,6 @@
 <?php
 
-
-
-class ConexionPDO
-{
+class ConexionPDO {
 
     private $motor;
     private $user;
@@ -11,107 +8,102 @@ class ConexionPDO
     private $host;
     private $db;
     private $pdo;
-    
-    private function conectarMySQL(){
+
+    private function conectarMySQL() {
 
         $this->motor = 'mysql';
         $this->user = 'postgres';
         $this->pass = 'bpid2017';
         $this->host = '181.225.96.71';
         $this->db = 'bpid';
-
-
     }
 
-    private function conectarPostgreSQL(){
+    private function conectarPostgreSQL() {
 
-        $this->motor ='pgsql';
+        //Servidor
+//        $this->motor ='pgsql';
+//        $this->user = 'postgres';
+//        $this->pass = 'bpid2017';
+//        $this->host = '181.225.96.71';
+//        $this->db = 'bpid';
+
+        //Local
+        $this->motor = 'pgsql';
         $this->user = 'postgres';
-        $this->pass = 'bpid2017';
-        $this->host = '181.225.96.71';
-        $this->db = 'bpid';
-       
-
+        $this->pass = '123456';
+        $this->host = '192.168.1.24';
+        $this->db = 'bpidl';
+        
     }
 
-    public function conectar($gestor){
-        
-        if($gestor==="MS")
+    public function conectar($gestor) {
+
+        if ($gestor === "MS")
             $this->conectarMySQL();
-        else if($gestor==="PG")
+        else if ($gestor === "PG")
             $this->conectarPostgreSQL();
 
         try {
-            
+
             $this->pdo = new PDO("$this->motor:host=$this->host;dbname=$this->db;", $this->user, $this->pass);
             $this->pdo->query("SET NAMES 'utf8'");
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-        }catch(PDOException $e){
+        } catch (PDOException $e) {
             echo 'Falló la conexión: ' . $e->getMessage();
         }
-
     }
 
-    public function  consultarValor($sql){
+    public function consultarValor($sql) {
 
         $rt = null;
-        
-        try
-            {
-            
+
+        try {
+
             $query = $this->pdo->prepare($sql);
             $query->execute();
-            if($res = $query->fetch(PDO::FETCH_OBJ))
-                $rt=$res->numero_completo;
-                    
-        }catch(PDOException $e){
-            error_log( $e->getMessage() ); 
+            if ($res = $query->fetch(PDO::FETCH_OBJ))
+                $rt = $res->numero_completo;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
         }
-        
+
         return $rt;
-        
     }
-    
-    public function consultar($sql){
+
+    public function consultar($sql) {
 
         $rt = null;
-    
+
         try {
-            
+
             $query = $this->pdo->prepare($sql);
             $query->execute();
             $rt = $query;
-               
-        }catch(PDOException $e){
-            error_log( $e->getMessage() ); 
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
         }
-        
+
         return $rt;
-        
     }
 
-    public function afectar($sql){
-      
-        $res=0;       
-      
-        try {
-       
-            $this->pdo->exec($sql);
-            $res=1;
+    public function afectar($sql) {
 
-        }catch(PDOException $e){
-            error_log( $e->getMessage() ); 
+        $res = 0;
+
+        try {
+
+            $this->pdo->exec($sql);
+            $res = 1;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
         }
-        
+
         return $res;
-        
-    } 
-  
-    public function cerrarConexion(){
-        
-        $this->pdo=null;
-        
+    }
+
+    public function cerrarConexion() {
+
+        $this->pdo = null;
     }
 
 }
