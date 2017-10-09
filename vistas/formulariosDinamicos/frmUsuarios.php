ee98e7373694a16153bfbc516b5c4e6113c852d0
Mauricio <pmaujob@gmail.com>
2017-10-09 18:06:07 -0500

    Signed-off-by: Mauricio <pmaujob@gmail.com>

---
+++ vistas/formulariosDinamicos/frmUsuarios.php
@@ -0,0 +1,51 @@
+<?php
+@session_start();
+
+$raiz = $_SESSION['raiz'];
+
+require_once $raiz . '/modelo/CargarUsuarios.php';
+
+if (!empty($_POST['value'])) {
+
+    $datos = $_POST['value'];
+    $res = CargarUsuarios::getUsuarios($datos);
+    ?>
+    <table>
+        <thead>
+            <tr><th>Cédula</th><th>Nombres</th><th>Apellidos</th><th>Agregar</th></tr>
+        </thead>
+        <tbody>
+            <?php
+            if (count($res) > 0) {
+                foreach ($res as $fila) {
+                    ?>
+
+                    <tr>
+                        <td><?php echo $fila[0]; ?></td>
+                        <td><?php echo $fila[1]; ?></td>
+                        <td><?php echo $fila[2]; ?></td>
+                        <td>
+                            <a href="#" title="Agregar">
+                                <div onclick="agregaru(<?php echo $fila[0]; ?>,<?php echo "'".$fila[1]."'"; ?>,<?php echo "'".$fila[2]."'"; ?>);">
+                                    <img style="width: 30px;" src="../../vistas/img/add.png">
+                                </div>
+                            </a>
+                        </td>
+                    </tr>
+
+                    <?php
+                }
+            } else {
+                ?>
+
+                <tr><td>No se encontraron resultados para la búsqueda.</td></tr>
+
+                <?php
+            }
+            ?>
+        </tbody>
+    </table>    
+
+    <?php
+}
+?>
